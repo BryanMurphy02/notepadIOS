87d4fb91a4f61c430de91b9f641b4302a8acee80
murphyb18atwit <murphyb18@wit.edu>
2022-10-15 05:46:56 -0400

    clear button not working

---
+++ notepadIOS/Assets/Scripts/ButtonControl.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ButtonControl : MonoBehaviour {
+
+    // public TMP_InputField inputField = GameObject.Find("InputField (TMP)").GetComponent<TMP_InputField>();
+    // // inputField = GameObject.Find("InputField (TMP)").GetComponent<TMP_InputField>();
+
+    // public void ClearText(){
+    //     // theText.GetComponent<TMP_InputField>().text = "";
+    //     // inputField.setTextWithoutNotfiy = "";
+    // }
+
+}
