0aa50717ab0519f68b73e8cb1f3de1e903134081
murphyb18atwit <murphyb18@wit.edu>
2022-10-16 16:30:34 -0400

    made the clear button work for text field

--- notepadIOS/Assets/Scripts/ButtonControl.cs
+++ notepadIOS/Assets/Scripts/ButtonControl.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+
+
 public class ButtonControl : MonoBehaviour {
 
-    // public TMP_InputField inputField = GameObject.Find("InputField (TMP)").GetComponent<TMP_InputField>();
-    // // inputField = GameObject.Find("InputField (TMP)").GetComponent<TMP_InputField>();
+    public TMP_InputField textField;
 
-    // public void ClearText(){
-    //     // theText.GetComponent<TMP_InputField>().text = "";
-    //     // inputField.setTextWithoutNotfiy = "";
-    // }
+
+    public void clearTextField(){
+        textField.text = "";
+    }
 
 }
