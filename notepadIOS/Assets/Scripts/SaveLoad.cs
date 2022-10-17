48947f74b35c6a6599834abb85b6387bfb7be38c
murphyb18atwit <murphyb18@wit.edu>
2022-10-17 00:22:26 -0400

    saving text

--- notepadIOS/Assets/Scripts/SaveLoad.cs
+++ notepadIOS/Assets/Scripts/SaveLoad.cs
@@ -2,14 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class SaveLoad : MonoBehaviour
 {
     public string theText;
-    public GameObject ourNote;
+    // public GameObject ourNote;
     public GameObject placeholder;
+    public TextMeshProUGUI ourNote;
+    public TMP_InputField userField;
 
     void Start(){
-        
+        theText = PlayerPrefs.GetString("NoteContents");
+        placeholder.GetComponent<TMP_InputField>().text = theText;
+    }
+
+    public void saveNote(){
+        theText = ourNote.text;
+        PlayerPrefs.SetString("NoteContents", theText);
     }
 }
