f95ed606cd9a4006c6726eb0304a52dde97e4ad3
murphyb18atwit <murphyb18@wit.edu>
2022-10-16 23:42:22 -0400

    maybe done working

---
+++ notepadIOS/Assets/Scripts/SaveLoad.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveLoad : MonoBehaviour
+{
+    public string theText;
+    public GameObject ourNote;
+    public GameObject placeholder;
+
+    void Start(){
+        
+    }
+}
