fdfacff3be3229d646860abbe54706d66563bc1d
murphyb18atwit <murphyb18@wit.edu>
2022-10-22 11:26:03 -0400

    added functionality to close button

--- notepadIOS/Assets/Scripts/ButtonControl.cs
+++ notepadIOS/Assets/Scripts/ButtonControl.cs
@@ -9,10 +9,23 @@ using TMPro;
 public class ButtonControl : MonoBehaviour {
 
     public TMP_InputField textField;
+    public GameObject thePanel;
 
 
     public void clearTextField(){
         textField.text = "";
+    }
+
+    public void cancelButton(){
+        thePanel.SetActive(false);
+    }
+
+    public void closeButton(){
+        thePanel.SetActive(true);
+    }
+
+    public void quitButton(){
+        Application.Quit();
     }
 
 }
